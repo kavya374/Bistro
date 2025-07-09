4edc497cf17943e3633c9c1229f4df93c071d383
kavya <kavyaajay89@gmail.com>
2025-07-09 10:41:58 +0530

    fix: make BASE_URL dynamic using env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
 // Change this to match your backend server port
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+//export const BASE_URL = "http://localhost:4000";
